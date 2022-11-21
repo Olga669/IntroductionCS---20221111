04ed79e937c358973645a3c7b5087f37fecaa864
Olga <olga992012@gmail.com>
2022-11-21 15:37:45 +0300

    Init

---
+++ 05/Program.cs
@@ -0,0 +1,8 @@
+﻿//05. С клавиатуры вводятся три числа. Найти максимальное из трех чисел.
+
+int a=6;
+int b=3;
+int c=9;
+int max;
+if (a>b) max=a; if (a>c) max=a; else max=c;
+System.Console.WriteLine($"Max digit is {max}");
