e712d1d0b374c85675121ab1d2f7d3f3cd65b567
Olga <olga992012@gmail.com>
2022-11-21 15:46:14 +0300

    Init

---
+++ 15/Program.cs
@@ -0,0 +1,6 @@
+﻿//15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что ее нет.
+//(Вывести: NO).
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (n<100) System.Console.WriteLine("NO");
+else System.Console.WriteLine(n/100%10);
