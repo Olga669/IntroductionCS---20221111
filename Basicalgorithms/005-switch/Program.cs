d03c3f82110c2776217848d0b07787dfa1de63b2
Olga <olga992012@gmail.com>
2022-12-03 01:57:43 +0300

    Lesson 4

---
+++ Basicalgorithms/005-switch/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводится число - день недели. Определить день недели.
+
+int day=-10;
+
+
+switch(day)
+{
+    case >0:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case <0:
+        System.Console.WriteLine("Вторник");
+        break;
+    default:
+    System.Console.WriteLine("Не правильный ввод");   
+        break;
+}
+
